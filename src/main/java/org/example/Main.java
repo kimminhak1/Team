0006ec0abf596fb3249f21ba76bab31a6367110e
kimminhak1 <alsgkrdlsla1@gmail.com>
2023-06-09 22:25:46 +0900

    셋팅2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("welcome!");
+        System.out.printf("kmh!");
 
 
     }
