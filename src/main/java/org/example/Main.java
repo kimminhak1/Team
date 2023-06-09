184f0481c422c4c61d48088121f5a7b9923e8265
kmh <kmh@kmhui-MacBookAir.local>
2023-06-09 23:44:02 +0900

    게시판 버젼 v 0.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,10 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("222!");
+        System.out.printf("== 게시판 v 0.1==");
+        System.out.printf("== 프로그램 시작 ==");
+        System.out.printf("명령");
+        System.out.printf("== 프로그램 종료 ==");
 
 
     }
