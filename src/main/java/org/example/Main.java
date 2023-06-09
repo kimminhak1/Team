b07dcf2ef900ab3d9055aee2d1311cd4f4601982
kmh <kmh@kmhui-MacBookAir.local>
2023-06-10 00:41:53 +0900

    게시물추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,21 +9,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int aitical = 0;
 
         System.out.println("== 게시판 v 0.1==");
         System.out.println("== 프로그램 시작 ==");
-        
-        
+
+
         while (true){
-        System.out.printf("명령) ");
-        String cmd = sc.nextLine();
-        if(cmd.equals("exit")){
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine();
+            if(cmd.equals("exit")){
             break;
+        } else if (cmd.equals("/user/article/write")) {
+            System.out.println("==게시물 등록==");
+            System.out.printf("제목 : ");
+            String title = sc.nextLine();
+            System.out.printf("내용 : ");
+            String  body = sc.nextLine();
+            int id = aitical + 1;
+            aitical=id;
+            System.out.printf("%d번 게시물이 입력 되었습니다.\n",id);
+        }else {
+            System.out.printf("입력된 명령어 : %s\n",cmd);
+         }
         }
-        System.out.printf("입력된 명령어 : %s\n",cmd);
-        }
-            System.out.println("== 프로그램 종료 ==");
-            sc.close();
+        System.out.println("== 프로그램 종료 ==");
+        sc.close();
     }
 }
