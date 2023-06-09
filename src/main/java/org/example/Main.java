824cfa6c4626303627cf02e66a1a235cd7649613
kimminhak1 <alsgkrdlsla1@gmail.com>
2023-06-09 23:24:20 +0900

    셋팅2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("111!");
+        System.out.printf("222!");
 
 
     }
