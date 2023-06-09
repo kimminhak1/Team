3ded21fe25f9423e6359b75fe1dd7ad3198ae750
kmh <kmh@kmhui-MacBookAir.local>
2023-06-10 00:05:56 +0900

    명령

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,23 @@
 package org.example;
 
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("== 게시판 v 0.1==");
-        System.out.printf("== 프로그램 시작 ==");
-        System.out.printf("명령");
-        System.out.printf("== 프로그램 종료 ==");
+
+         Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 게시판 v 0.1==");
+        System.out.println("== 프로그램 시작 ==");
+        System.out.printf("명령) ");
+        String cmd = sc.nextLine();
+        System.out.printf("입력된 명령어 : %s\n",cmd);
+        System.out.println("== 프로그램 종료 ==");
+
 
 
     }
